5a2150056d12c7e5ec5453fb6fb4e60343d7e97a
Ptvr <pauline.tavernier@epitech.eu>
2023-10-28 16:57:33 +0200

    [+] Adding front login page

--- lib/components/my_button.dart
+++ lib/components/my_button.dart
@@ -1,29 +1,40 @@
 import 'package:flutter/material.dart';
 
 class MyButton extends StatelessWidget {
-
   final Function()? onTap;
 
-  const MyButton({super.key, required this.onTap});
+  const MyButton({Key? key, required this.onTap});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: onTap,
       child: Container(
-        padding: const EdgeInsets.all(25),
+        padding: const EdgeInsets.all(15),
         margin: const EdgeInsets.symmetric(horizontal: 25),
         decoration: BoxDecoration(
-          color: Colors.black,
           borderRadius: BorderRadius.circular(8),
+          gradient: LinearGradient(
+            colors: [Colors.orange, Colors.deepOrange],
+            begin: Alignment.centerLeft,
+            end: Alignment.centerRight,
+          ),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.deepOrange.withOpacity(0.3),
+              spreadRadius: 3,
+              blurRadius: 5,
+              offset: Offset(0, 3),
+            ),
+          ],
         ),
-        child: const Center(
+        child: Center(
           child: Text(
             'Sign In',
             style: TextStyle(
               color: Colors.white,
               fontWeight: FontWeight.bold,
-              fontSize: 16,
+              fontSize: 18,
             ),
           ),
         ),
