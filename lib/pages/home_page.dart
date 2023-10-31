ea2c452ec5b77120df60efd1ad4f61f08b8245ba
Ptvr <pauline.tavernier@epitech.eu>
2023-10-31 09:40:50 +0100

    adding landingpage front

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:ready_home_chef/components/SidebarDrawer.dart';
+import 'package:ready_home_chef/components/landing_page.dart';
 
 class HomePage extends StatelessWidget {
   HomePage({super.key});
